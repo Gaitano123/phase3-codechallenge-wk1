58f40bd40112990a3b8ef994f265a330511ea5d2
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-20 18:22:04 +0300

    completed clock.py

---
+++ clock.py
@@ -0,0 +1,6 @@
+def clock_converter(hour, minute, period):
+    if period == "am" and hour == 12:
+        hour = 0
+    elif period == "pm" and hour != 12:
+        hour += 12
+    return f"{hour:02d}{minute:02d}"  
