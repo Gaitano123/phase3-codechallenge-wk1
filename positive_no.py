69a4d66197eaf5a7a0f8e35ab9e6ee745c26cd4e
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-20 22:13:16 +0300

    Completed on the positive checker

---
+++ positive_no.py
@@ -0,0 +1,13 @@
+def positive_counter(a, b,c):
+    positive_number = 0
+    
+    if a > 0:
+        positive_number += 1
+        
+    if b > 0:
+        positive_number += 1
+        
+    if c > 0:
+        positive_number += 1
+        
+    return positive_number == 2
